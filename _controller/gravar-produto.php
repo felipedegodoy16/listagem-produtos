72a06b1651681478097f730a8decf866b16912de
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-01-21 21:38:41 -0300

    criando o controller

--- _controller/gravar-produto.php
+++ _controller/gravar-produto.php
@@ -3,3 +3,16 @@
     include_once "../_class/Produto.php";
 
     $produto = new Produto();
+
+    $produto->setNome($_POST['nome']);
+    $produto->setDescricao($_POST['desc']);
+    $produto->setValor($_POST['valor']);
+    if($_POST['options'] === 'Sim') {
+        $produto->setDisp(true);
+    } else {
+        $produto->setDisp(false);
+    }
+
+    var_dump($produto);
+    
+?>
