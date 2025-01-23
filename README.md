4b13e56e851619903ef6d8fd6161d83ce237b480
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-01-23 12:45:14 -0300

    adicionando o sql e acertando o readme

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ Projeto simples com o intuito de fazer um pequeno cadastro de produtos e listá-
 ---
 
 Link do meu [github](https://github.com/felipedegodoy16) com mais projetos como esse.
+
+---
+
+### Instruções para uso do sistema
+
+ Para conseguir rodar o projeto em sua máquina siga as seguintes etapas:
+
+ - Tenha o XAMPP instalado ou algum outro software que permita que sua máquina se torne um servidor Apache com suporte a MySQL;
+ - Faça o download do projeto em sua máquina;
+ - Extraia o arquivo .zip que será baixado;
+ - Copie a pasta extraída para o caminho no qual você possa acessar através do servidor Apache instalado, no caso do XAMPP 'C:\xampp\htdocs';
+ - Abra o servidor de Banco de Dados do seu servidor local e importe o arquivo sql que se encontra na pasta '_sql';
+ - E está feito;
+ - Tire bons proveitos de meu software.
---
+++ _sql/proj-listagem.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 23/01/2025 às 16:42
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `proj-listagem`
+--
+CREATE DATABASE IF NOT EXISTS `proj-listagem` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `proj-listagem`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` bigint(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `valor` float NOT NULL,
+  `disponibilidade` char(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
